b33f6ceff24c3265c76d5974f544d8b2b35491a6
BuildTools <unconfigured@null.spigotmc.org>
2024-02-01 19:19:21 +0100

    edit maison

---
+++ liste_pieces.py
@@ -0,0 +1,74 @@
+
+
+piece01 = [[1]]
+
+piece02 = [[0, 1, 0],
+           [1, 1, 1],
+           [0, 1, 0]]
+
+piece03 = [[1, 1, 1, 1, 1]]
+
+piece04 = [[1, 1]]
+
+piece05 = [[1, 0, 0],
+           [1, 1, 1], 
+           [0, 0, 1]]
+
+piece06 = [[1, 1],
+           [1, 0]]
+
+piece07 = [[1, 1],
+           [1, 1]]
+
+piece08 = [[1, 1, 1],
+           [1, 1, 0]]
+
+piece09 = [[1, 1, 1, 1],
+           [0, 0, 1, 0]]
+
+piece10 = [[1, 1, 1],
+           [0, 1 ,0]]
+
+piece11 = [[0, 1, 1],
+           [1, 1, 0]]
+
+piece12 = [[0, 1, 1],
+           [1, 1, 0],
+           [0, 1, 0]]
+
+piece13 = [[1, 0],
+           [1, 0],
+           [1, 1]]
+
+piece14 = [[1, 1, 1]]
+
+piece15 = [[1, 1, 1, 1]]
+
+piece16 = [[1, 1, 1],
+           [1, 0, 0],
+           [1, 0, 0]]
+
+piece17 = [[1, 1, 1],
+           [1, 0, 1]]
+
+piece18 = [[1, 1, 1, 1],
+           [1, 0, 0, 0]]
+
+piece19 = [[1, 1, 1, 0],
+           [0, 0, 1, 1]]
+
+piece20 = [[1, 1, 0],
+           [0, 1, 1],
+           [0, 0, 1]]
+
+piece21 = [[1, 1, 1],
+           [0, 1, 0],
+           [0, 1, 0]]
+
+total_piece = [piece01, piece02, piece03, piece04, piece05, piece06, piece07, piece08, piece09, piece10, piece11, piece12, piece13, piece14, piece15, piece16, piece17, piece18, piece19, piece20, piece21]
+
+map_empty = [[0]*20 for _ in range(20)]
+for i in range(len(map_empty)):
+    print(map_empty[i])
+    
+
---
+++ main.py
@@ -0,0 +1,4 @@
+import liste_pieces
+
+
+pi
